7c3fa6eafce04ae0c9848bf4a361400319d0aacd
christianpasinrey <70205361+christianpasinrey@users.noreply.github.com>
2022-06-10 06:29:39 +0200

    PWA

---
+++ config/laravelpwa.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'name' => 'CPR PWA',
+    'manifest' => [
+        'name' => env('APP_NAME', 'App'),
+        'short_name' => 'PWA',
+        'start_url' => '/',
+        'background_color' => '#ffffff',
+        'theme_color' => '#000000',
+        'display' => 'browser',
+        'orientation'=> 'any',
+        'status_bar'=> 'black',
+        'icons' => [
+            '512x512' => [
+                'path' => 'public/storage/app-svgrepo-com.png',
+                'purpose' => 'any'
+            ],
+        ],
+        'custom' => []
+    ]
+];
