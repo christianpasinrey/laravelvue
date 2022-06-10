dd5669e96c184f4368143723d9c81af9d1d173a0
christianpasinrey <70205361+christianpasinrey@users.noreply.github.com>
2022-06-10 02:29:33 +0200

    Task Model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,4 +9,26 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Task extends Model
 {
     use HasFactory;
+    protected $table="tasks";
+
+    protected $fillable = [
+        'text',
+        'limit_date',
+        'user_id'
+    ];
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'taskables',
+            'task_id',
+            'receiver_id',
+            'id',
+            'id'
+        );
+    }
 }
